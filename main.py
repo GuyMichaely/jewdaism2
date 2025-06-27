68959c2bdec13ce21d20b3d6c330607d6fbf9c6e
GuyMichaely <guymichaely@gmail.com>
2025-06-27 18:06:55 -0400

    revert

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from flask.helpers import redirect
 app = Flask(__name__)
 
 REMOTE_FILE_URL = 'https://scontent-atl3-1.xx.fbcdn.net/v/t1.6435-9/62305197_689453418177959_639826170052870144_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=bx07z8CxhGgQ7kNvwHewC0Z&_nc_oc=Adkrhoet9i67f_K66EHpYK4kpUYkoGl09D12WW7r1bUkQMM81RIIV7XGtVTdZfa0jAor3WFBTxrOk9FAie9LI2OI&_nc_zt=23&_nc_ht=scontent-atl3-1.xx&_nc_gid=jDXBdaGSfdO33b64B1Lgwg&oh=00_AfNNL9qMPOdMP3wixAx2rCWbjIyPsfT9ekHCw-3uj5V4UA&oe=68866E0A'
-SECRET = '/guy_michaely_owns_jewdaism_K66EHpYK4kpUY'
+SECRET = 'guy_michaely_owns_jewdaism_K66EHpYK4kpUY'
 
-@app.route(SECRET_PATH)
+'''@app.route(SECRET_PATH)
 def proxy_file():
     return redirect(REMOTE_FILE_URL)
     r = requests.get(REMOTE_FILE_URL, stream=True)
@@ -16,13 +16,12 @@ def proxy_file():
     return Response(
         r.iter_content(chunk_size=8192),
         content_type=r.headers.get('Content-Type')
-    )
+    )'''
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     # abort(404)
-    return '', 404
     ref = request.referrer
     if ref and any(v in ref for v in [SECRET, 'guytest']):
         print(f'{ref=}')
